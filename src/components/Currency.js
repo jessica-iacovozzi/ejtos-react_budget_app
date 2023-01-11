d300c6faa087f0650435318fa6c729c720c29780
jessica-iacovozzi <iacovozzi.jessica@gmail.com>
2023-01-11 11:26:32 -0500

    only missing styled dropdown

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,4 +1,4 @@
-import React, { useState,useContext,useEffect } from 'react';
+import React, { useState, useContext, useEffect, useCallback } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
@@ -6,31 +6,30 @@ const Budget = () => {
     const { dispatch } = useContext(AppContext);
 
     const handleChange = (event) => {
-        setCurrency(event.target.value);
-        changeCurrency()
-      };
+      setCurrency(event.target.value);
+    };
 
-      const changeCurrency = () => {
-        dispatch({
-            type: 'CHG_CURRENCY',
-            payload: currency,
-        });
-      }
-
-   useEffect(() => {
-        console.log(currency);
-    }, [currency] )
+    const changeCurrency = useCallback(() => {
+      dispatch({
+          type: 'CHG_CURRENCY',
+          payload: currency,
+      });
+      }, [currency, dispatch])
+      
+    useEffect(() => {
+       changeCurrency()
+    }, [changeCurrency] )
 
     return (
         <div className="alert alert-secondary">
-        <select id="inputGroupSelect02" onChange={handleChange}>
+            <select id="inputGroupSelect02" onChange={handleChange}>
                 <option defaultValue>Currency</option>
                 <option value="$" name="dollar">$ Dollar</option>
                 <option value="£" name="pound">£ Pound</option>
                 <option value="€" name="euro">€ Euro</option>
                 <option value="₹" name="ruppee">₹ Ruppee</option>
-                  </select>
-                  </div>
+            </select>
+        </div>
     );
 };
 
